c95aff1ba6963254e6f70e9f681bb5400dcd7c83
abtjade <abt.jade@gmail.com>
2021-06-07 00:04:44 +0900

    Set project

---
+++ bucketlist/src/test/java/kr/ac/jejunu/bucketlist/BucketlistApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.bucketlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
