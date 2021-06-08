593915d7616ea33dd6d63477f3c0d0684aa37dad
abtjade <abt.jade@gmail.com>
2021-06-09 04:17:11 +0900

    로그아웃 기능 구현 - session invalidate

--- bucketlist/src/main/java/kr/ac/jejunu/bucketlist/LoginController.java
+++ bucketlist/src/main/java/kr/ac/jejunu/bucketlist/LoginController.java
@@ -2,10 +2,7 @@ package kr.ac.jejunu.bucketlist;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -28,5 +25,10 @@ public class LoginController {
         } else {
             return null;
         }
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpSession session) {
+        session.invalidate();
     }
 }
